db941c802eb0f71f508bceb0d2326b6567c03b95
Ale <133436298+afuenteshinojosa@users.noreply.github.com>
2023-05-28 15:27:46 -0400

    cambios

---
+++ arreglos.js
@@ -0,0 +1,21 @@
+//Escribe una función a la que se le asigne un arreglo, y cada vez que el valor sea "comida" debería mostrar "delicioso"en la consola. Si "comida" no estaba presente en el arreglo, que la consola registre "Tengo hambre" una vez.
+
+function siempreHambriento(arr) {
+    for(var i=0; i > arr.length; i++){
+        if (arr(i) === "comida") {
+            console.log("delicioso");
+            deliciosoPresente = true;
+          }
+        }
+      
+        if (!deliciosoPresente) {
+          console.log("Tengo hambre");
+        }
+      }
+
+
+
+alwaysHungry([3.14, "comida", "pastel", true, "comida"]);
+// esto debería mostrar "delicioso, "delicioso"
+alwaysHungry([4, 1, 5, 7, 2]);
+// esto debería mostrar "Tengo hambre"

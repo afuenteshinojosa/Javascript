f91e684d39666415bcf2861f990304df322748b2
Ale <133436298+afuenteshinojosa@users.noreply.github.com>
2023-05-29 12:56:37 -0400

    MeGusta

---
+++ Me-gusta/js/MeGusta.js
@@ -0,0 +1,26 @@
+var count= 9;
+var countElement= document.querySelector(".NeilLikes");
+
+function Likes(){
+    count++;
+    countElement.innerText = count + " Likes"
+    console.log(count);
+}
+
+var count2= 12;
+var countElement2= document.querySelector(".NicholeLikes");
+
+function Likes2(){
+    count2++;
+    countElement2.innerText = count2 + " Likes"
+    console.log(count2);
+}
+
+var count3= 9;
+var countElement3= document.querySelector(".JimLikes");
+
+function Likes3(){
+    count3++;
+    countElement3.innerText = count3 + " Likes"
+    console.log(count3);
+}

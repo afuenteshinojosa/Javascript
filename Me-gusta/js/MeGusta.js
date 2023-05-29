f7fc76563c02705f70c75be50c0dc9faf80e8b77
Ale <133436298+afuenteshinojosa@users.noreply.github.com>
2023-05-29 14:02:26 -0400

    cambios

--- Me-gusta/js/MeGusta.js
+++ Me-gusta/js/MeGusta.js
@@ -3,7 +3,7 @@ var countElement= document.querySelector(".NeilLikes");
 
 function Likes(){
     count++;
-    countElement.innerText = count + " Likes"
+    countElement.innerText = count + " Like(s)"
     console.log(count);
 }
 
@@ -12,7 +12,7 @@ var countElement2= document.querySelector(".NicholeLikes");
 
 function Likes2(){
     count2++;
-    countElement2.innerText = count2 + " Likes"
+    countElement2.innerText = count2 + " Like(s)"
     console.log(count2);
 }
 
@@ -21,6 +21,6 @@ var countElement3= document.querySelector(".JimLikes");
 
 function Likes3(){
     count3++;
-    countElement3.innerText = count3 + " Likes"
+    countElement3.innerText = count3 + " Like(s)"
     console.log(count3);
 }
